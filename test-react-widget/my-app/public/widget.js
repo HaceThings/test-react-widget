39cc4ad99e4d6d12d961a8cacd3a8f020f5ae59a
Jace Holton <jaceholton@gmail.com>
2025-01-29 14:48:08 -0800

    Fix formatting

--- test-react-widget/my-app/public/widget.js
+++ test-react-widget/my-app/public/widget.js
@@ -4,8 +4,8 @@ import { createRoot } from "react-dom/client";
 const Blob = () => {
     return (
         <div className="App">
-        <img alt="a pink square with a smiley face on it" title="party blob hangouts blob" loading="lazy" src="https://emojis.slackmojis.com/emojis/images/1643514770/7808/party-blob.gif?1643514770"></img>
-      </div>
+            <img alt="a pink square with a smiley face on it" title="party blob hangouts blob" loading="lazy" src="https://emojis.slackmojis.com/emojis/images/1643514770/7808/party-blob.gif?1643514770"></img>
+        </div>
     )
 }
 
