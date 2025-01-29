cc413712091f1d7312d54ae596f695b7ce23888d
Jace Holton <jaceholton@gmail.com>
2025-01-29 14:41:53 -0800

    Update widget/index to render blob

--- test-react-widget/my-app/public/widget.js
+++ test-react-widget/my-app/public/widget.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { createRoot } from "react-dom/client";
 
-import App from '../src/App';
+const Blob = () => {
+    return (
+        <div className="App">
+        <img alt="a pink square with a smiley face on it" title="party blob hangouts blob" loading="lazy" src="https://emojis.slackmojis.com/emojis/images/1643514770/7808/party-blob.gif?1643514770"></img>
+      </div>
+    )
+}
 
-return App;
+const mountChatWidget = () => {
+    const chatDiv = document.createElement("div");
+    document.body.appendChild(chatDiv);
+    const root = createRoot(chatDiv);
+    root.render(<Blob />);
+}
+
+window.mountChatWidget = mountChatWidget;
