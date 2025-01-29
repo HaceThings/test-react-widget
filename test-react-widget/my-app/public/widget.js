fe92f39aa762b516094f26656f35e8aa0f6d0174
Jace Holton <jaceholton@gmail.com>
2025-01-29 15:06:13 -0800

    Explicity define options

--- test-react-widget/my-app/public/widget.js
+++ test-react-widget/my-app/public/widget.js
@@ -10,14 +10,14 @@ const Blob = () => {
 
 const mountChatWidget = () => {
     const chatDiv = document.createElement("div");
-    const blob = document.createElement("img", options={
-        alt: "a pink square with a smiley face on it",
-        title: "party blob hangouts blob",
-        loading: "lazy",
-        src: "https://emojis.slackmojis.com/emojis/images/1643514770/7808/party-blob.gif?1643514770"
-    });
+    const blob = document.createElement("img");
+    blob.alt = "a pink square with a smiley face on it";
+    blob.title = "party blob hangouts blob";
+    blob.loading = "lazy";
+    blob.src = "https://emojis.slackmojis.com/emojis/images/1643514770/7808/party-blob.gif?1643514770";
     chatDiv.appendChild(blob);
     document.body.appendChild(chatDiv);
+    console.log("Appended child to body");
 }
 
 window.mountChatWidget = mountChatWidget;
