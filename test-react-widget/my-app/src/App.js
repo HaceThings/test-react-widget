d494ecdc0eee3290cd1ba3a7e171e5efdd49a21f
Jace Holton <jaceholton@gmail.com>
2025-01-29 11:59:32 -0800

    Add my-app

---
+++ test-react-widget/my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img alt="a pink square with a smiley face on it" title="party blob hangouts blob" loading="lazy" src="https://emojis.slackmojis.com/emojis/images/1643514770/7808/party-blob.gif?1643514770"></img>
+    </div>
+  );
+}
+
+export default App;
